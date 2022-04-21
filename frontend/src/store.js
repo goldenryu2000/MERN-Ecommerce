bdc856f092cfa63843e3ee327fa59bf6a19d4dad
Nishant Sharma <goldenryu2000@gmail.com>
2022-04-22 00:52:45 +0530

    Finished implementing states using redux
    created states using redux namely 6 states for homeScreen and ProductScreen respectively :
    PRODUCT_LIST_REQUEST
    PRODUCT_LIST_SUCCESS
    PRODUCT_LIST_FAIL
    
    &
    PRODUCT_DETAILS_REQUEST
    PRODUCT_DETAILS_SUCCESS
    PRODUCT_DETAILS_FAIL

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,11 +4,15 @@ import {
   applyMiddleware,
 } from "redux";
 import thunk from "redux-thunk";
-import { productListReducer } from "./reducers/productReducers";
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 const initialState = {};
 const middleware = [thunk];
