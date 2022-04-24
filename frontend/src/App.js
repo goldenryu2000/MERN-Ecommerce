6d0b992dbb2d3b895ba67f83ca8c085c9d100d94
Nishant Sharma <goldenryu2000@gmail.com>
2022-04-25 00:13:16 +0530

    Started working on Frontend for User Auth
    Added the actions and reducers for user auth and a form container for user login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 const App = () => {
   return (
     <BrowserRouter>
@@ -13,10 +14,11 @@ const App = () => {
       <Container>
         <main className="py-3">
           <Routes>
-            <Route exact path="/" element={<HomeScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
             <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/cart/:id" element={<CartScreen />} />
             <Route path="/cart" element={<CartScreen />} />
+            <Route exact path="/" element={<HomeScreen />} />
           </Routes>
         </main>
       </Container>
